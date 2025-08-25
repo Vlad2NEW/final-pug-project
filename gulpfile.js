0944d3b1bafec7625ca7c7e736a427080822c2f8
Vlad2NEW <149104771+Vlad2NEW@users.noreply.github.com>
2025-08-25 16:49:24 +0300

    Adding build task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,11 @@ const pug = require("gulp-pug");
 gulp.task("pug-compile", () => {
     return gulp.src(["app/pug/**/*.pug", "!app/pug/includes/**/*.pug"])
     .pipe(pug({pretty: true}))
-    .pipe(gulp.dest("app/html"))
+    .pipe(gulp.dest("app/html")) // залишаємо локально тут
 });
 
 gulp.task("watch", () => {
     gulp.watch("app/pug/**/*.pug", gulp.series("pug-compile"))
 });
+
+gulp.task('build', gulp.series('pug-compile'));
